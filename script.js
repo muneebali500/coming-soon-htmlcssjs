cc3d1acf5f97436fb3482dd1bca683b159c18644
Muneeb Ali <muneebali500@gmail.com>
2021-11-08 18:16:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// EVENT LISTENERS
+const btnNotify = document.getElementById(`btn-notify`);
+const modal = document.getElementById(`modal`);
+const closeBtn = document.getElementById(`btn-close`);
+const inputEl = document.getElementById(`input`);
+const form = document.getElementById(`form`);
+
+btnNotify.addEventListener(`click`, openModal);
+form.addEventListener(`submit`, sendNotification);
+closeBtn.addEventListener(`click`, closeModal);
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function sendNotification(e) {
+  e.preventDefault();
+
+  inputEl.value = ``;
+  alert(
+    `Thanks for the Subscription. You will be notified when the Website is ON`
+  );
+}
+
+function closeModal(e) {
+  modal.style.display = `none`;
+}
+
+/*----------------------------------------- SETTING COUNTDOWN -------------------------------------------------*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const daysElement = document.querySelector(`[data-days]`);
+const hoursElement = document.querySelector(`[data-hours]`);
+const minutesElement = document.querySelector(`[data-minutes]`);
+const secondsElement = document.querySelector(`[data-seconds]`);
+
+const countDate = new Date(`Dec 15, 2021 00:00:00`).getTime();
+
+function countDown() {
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  daysElement.innerText = Math.floor(gap / days);
+  hoursElement.innerText = Math.floor((gap % days) / hours);
+  minutesElement.innerText = Math.floor((gap % hours) / minutes);
+  secondsElement.innerText = Math.floor((gap % minutes) / seconds);
+}
+
+setInterval(countDown);
